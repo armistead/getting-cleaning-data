467314279f32389c074d0e41d00df99a99d44ec8
armistead <johnawilson@gmail.com>
2017-03-08 23:33:08 -0600

    Cleaned up
    I took another pass at the assignment, incorporating ddplyr, plyr, and the table of averages by Activity Type.

--- run.analysis.R
+++ run.analysis.R
@@ -1,5 +1,11 @@
-#set working directory to the location where you unzipped the data
-setwd("C:/Users/johnarmistead/Data Sci Coursera/Class3/UCI HAR Dataset")
+## start here
+setwd("C:/Users/user/Data Sci Coursera/Class3/UCI HAR Dataset")
+
+install.packages("plyr") ## for the use of ddply at the end
+library(plyr)
+
+install.packages("dplyr") # for the use of inner join and select
+library(dplyr)
 
 # Read data
 features     <- read.table('./features.txt',header=FALSE); #imports features.txt
@@ -11,7 +17,7 @@ subjectTest <- read.table('./test/subject_test.txt',header=FALSE); #imports subj
 xTest       <- read.table('./test/x_test.txt',header=FALSE); #imports x_test.txt
 yTest       <- read.table('./test/y_test.txt',header=FALSE); #imports y_test.txt
 
-# Give column names to data
+# Give column names to the raw data
 colnames(activityType)  <- c('activityId','activityType');
 colnames(subjectTrain)  <- "subjectId";
 colnames(xTrain)        <- features[,2]; 
@@ -20,40 +26,33 @@ colnames(subjectTest) <- "subjectId";
 colnames(xTest)       <- features[,2]; 
 colnames(yTest)       <- "activityId";
 
-# Get rid of all the other columns without mean or st dev data
+# create a subset with only the column names you want
 data <- rbind(cbind(yTrain,subjectTrain,xTrain), cbind(yTest,subjectTest,xTest))
-colNames <- colnames(data)
-subset = (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames))
-almost <- data[subset==TRUE]
-finally <- merge(almost, activityType, by='activityId', all.x = TRUE)
-colNames <- colnames(finally)
+        names(data) <- make.unique(names(data)) # make them unique
+        subset <- select(data, activityId, subjectId, contains("-mean()", ignore.case = TRUE), contains("-std()", ignore.case = TRUE))
 
-for (i in 1:length(colNames)) 
-{
-        colNames[i] <- gsub("\\()","",colNames[i])
-        colNames[i] <- gsub("-std$","StdDev",colNames[i])
-        colNames[i] <- gsub("-mean","Mean",colNames[i])
-        colNames[i] <- gsub("^(t)","Time",colNames[i])
-        colNames[i] <- gsub("^(f)","Freq",colNames[i])
-        colNames[i] <- gsub("([Gg]ravity)","Gravity",colNames[i])
-        colNames[i] <- gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
-        colNames[i] <- gsub("[Gg]yro","Gyro",colNames[i])
-        colNames[i] <- gsub("AccMag","AccMagnitude",colNames[i])
-        colNames[i] <- gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
-        colNames[i] <- gsub("JerkMag","JerkMagnitude",colNames[i])
-        colNames[i] <- gsub("GyroMag","GyroMagnitude",colNames[i])
-}
+        colNames <- colnames(subset) ## break out column names
+        for (i in 1:length(colNames)) ## clean up junk characters in column headers
+        {
+                colNames[i] <- gsub("\\()","",colNames[i])
+                colNames[i] <- gsub("-std$","StdDev",colNames[i])
+                colNames[i] <- gsub("-mean$","Mean",colNames[i])
+                colNames[i] <- gsub("^(t)","Time",colNames[i])
+                colNames[i] <- gsub("^(f)","Freq",colNames[i])
+                colNames[i] <- gsub("([Gg]ravity)","Gravity",colNames[i])
+                colNames[i] <- gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
+                colNames[i] <- gsub("[Gg]yro","Gyro",colNames[i])
+                colNames[i] <- gsub("AccMag","AccMagnitude",colNames[i])
+                colNames[i] <- gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
+                colNames[i] <- gsub("JerkMag","JerkMagnitude",colNames[i])
+                colNames[i] <- gsub("GyroMag","GyroMagnitude",colNames[i])
+        }
+        colnames(subset) <- colNames ## put column names back in
+        subberset <- inner_join(subset, activityType, by='activityId') ## bring in Activity Type names
 
-colnames(finally) <- colNames
+        tidy <- select(subberset, activityId, subjectId, activityType, everything()) ## put activity Type details in the far left column.
+        average <- ddply(tidy, .(activityType), function(x) colMeans(x[, 4:68])) ## average all variables by the Activity Type factors
 
-laststep  <- finally[,names(finally) != 'activityType']
-
-tidy    = aggregate(laststep[,names(laststep) != c('activityId','subjectId')],by=list(activityId=laststep$activityId,subjectId = laststep$subjectId),mean)
-
-# Merge tidy with activityType with descriptions
-tidy    = merge(tidy,activityType,by='activityId',all.x=TRUE)
-
-# Export 
-write.table(tidy, './tidyData.txt',row.names=TRUE,sep='\t')
-
-#enjoy
+## export
+write.table(tidy, './tidydata.txt',row.names=TRUE,sep='\t') ## there you have the tidy set.
+write.table(average, "./averages.txt", row.names=TRUE,sep='\t')
